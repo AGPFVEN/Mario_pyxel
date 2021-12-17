8c4708ede3efeae3a4b711a2b3b7f0ea57f8a04b
Alfonso <alfonso_pineda2003@hotmail.com>
2021-12-17 21:20:30 +0100

    enemy

--- enemy.py
+++ enemy.py
@@ -1,4 +1,6 @@
+from collisionManager import Collision_manager
 from dynamic_object import Dynamic_object
+from mario import Mario
 
 class Enemy(Dynamic_object):
     pass
@@ -8,9 +10,11 @@ class Enemy(Dynamic_object):
             self.acceleration_x = 1
 
         if len(self.collading_left) > 0:
+            #self.sprite[3] *= -1
             self.acceleration_x = 1
         
         if len(self.collading_right) > 0:
+            #self.sprite[3] *= -1
             self.acceleration_x = -1
 
         self.x += self.acceleration_x
